62a5359f5b62a5481e1d515745aca3cec79534cb
Martin Gondermann <mgondermann@np4.net>
2013-08-15 10:12:13 +0200

    Initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,11 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Thunderbird to /Applications.
+#
+# Examples
+#
+#   include thunderbird
+class thunderbird($locale = 'en-US'){
+  package { 'Thunderbird':
+    source   => "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/17.0.8/mac/${locale}/Thunderbird%2017.0.8.dmg",
+    provider => 'appdmg'
+  }
 }
---
+++ spec/classes/thunderbird_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'thunderbird' do
+it do
+    should contain_class('thunderbird')
+    should contain_package('Thunderbird').with({
+      :source   => "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/17.0.8/mac/en-US/Thunderbird%2017.0.8.dmg",
+      :provider => 'appdmg'
+    })
+  endend
